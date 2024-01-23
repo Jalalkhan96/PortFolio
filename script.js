54403402c88a26c0249d228327e630d0a3b8cb9f
KHAN <135963102+Jalalkhan96@users.noreply.github.com>
2024-01-23 16:12:23 +0500

    Update script.js
    Some Fixing

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
+const btnPopup = document.querySelector('.butlogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const formBoxRegister = document.querySelector('.form-box.Register');
+const formBoxLogin = document.querySelector('.form-box.login');
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
+    formBoxLogin.style.transform = 'translateX(-400px)';
+    formBoxRegister.style.transform = 'translateX(0)';
 });
 
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
+    formBoxLogin.style.transform = 'translateX(0)';
+    formBoxRegister.style.transform = 'translateX(400px)';
 });
-btnPopup.addEventListener('click', () => {wrapper.classList.add('active-popup');
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
 });
-iconClose.addEventListener('click', () => {  wrapper.classList.remove('active-popup'); });
 
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const wrapper = document.querySelector('.wrapper');
-//     const loginLink = document.querySelector('.login-link');
-//     const registerLink = document.querySelector('.register-link');
-//     const btnPopup = document.querySelector('.btnLogin-popup');
-//     const iconClose = document.querySelector('.icon-close');
-
-//     registerLink.addEventListener('click', () => {
-//         wrapper.classList.add('active');
-//     });
-
-//     loginLink.addEventListener('click', () => {
-//         wrapper.classList.remove('active');
-//     });
-
-//     btnPopup.addEventListener('click', () => {
-//         wrapper.classList.add('active-popup');
-//     });
-
-//    
-// });
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+    formBoxLogin.style.transform = 'translateX(-400px)';
+    formBoxRegister.style.transform = 'translateX(400px)';
+});
